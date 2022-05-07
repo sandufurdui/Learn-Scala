0199b5c8d4b4016fa3068177669b4338de6eb612
Alex Furdui <furdui.alexandru@isa.utm.md>
2022-05-07 20:42:27 +0300

    update

--- producer/src/main/scala/prod/TcpServer.scala
+++ producer/src/main/scala/prod/TcpServer.scala
@@ -4,7 +4,6 @@ import java.net.InetSocketAddress
 import akka.actor.Actor
 import akka.io.{IO, Tcp}
 import akka.actor.Props
-import prod.SimplisticHandler
 
 object TcpServer {
   def props(remote: InetSocketAddress): Props =
@@ -24,11 +23,6 @@ class TcpServer(remote: InetSocketAddress) extends Actor {
 
     case CommandFailed(_: Bind) ⇒ context stop self
 
-    case c @ Connected(remote, local) =>
-      println(s"Client connected - Remote(Client): ${remote.getAddress} Local(Server): ${local.getAddress}")
-      val handler = context.actorOf(Props[SimplisticHandler])
-      val connection = sender()
-      connection ! Register(handler)
   }
 
 }
