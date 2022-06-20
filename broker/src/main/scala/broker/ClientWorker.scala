2abf953dc9ea550e6214ab3f248528d0a3562d78
Alex Furdui <furdui.alexandru@isa.utm.md>
2022-06-21 01:01:31 +0300

    subscriber message publish
    now subscriber besides getting messages which is subscribed to, it can also publish by entering the post mode and can return to get messages by returning to get mode

--- broker/src/main/scala/broker/ClientWorker.scala
+++ broker/src/main/scala/broker/ClientWorker.scala
@@ -3,7 +3,7 @@ package broker
 import akka.actor.{Actor, ActorRef}
 import akka.io.Tcp
 import akka.util.ByteString
-
+import play.api.libs.json.{JsNull, JsValue, Json, JsObject}
 import scala.collection.mutable.{ArrayBuffer, ListBuffer}
 import scala.language.postfixOps
 
@@ -28,6 +28,7 @@ case class ToClient(response: String)
    var globalAckKey = 0
    var firstWord = ""
    var lastWord = ""
+   val idList = new ArrayBuffer[Int]()
    val responseAsBuff = new ArrayBuffer[String]
    var responseAsList: List[String] = List[String]()
   println(s"------------client worker started ${context.self}------------")
@@ -36,18 +37,33 @@ case class ToClient(response: String)
     case qResponse(str) =>
       println(s"(client worker) queueManager response ${str}")
       val r = scala.util.Random
-      val keyGenerator = r.nextInt(100)
+      val keyGenerator = r.nextInt(1000)
       globalAckKey = keyGenerator
       val testString = s"{\n    \"key\": ${keyGenerator},\n    \"message\": \"${str}\"\n}"
+      idList.append(keyGenerator)
         TCPsender ! Write(ByteString(testString))
 
     case Received(data) => {
+      val dataDecoded = data.utf8String
+      val json: JsValue = Json.parse(dataDecoded)
+      val command = (json \ "command").getOrElse(JsNull).toString().replaceAll("\"", "")
+      val mode = (json \ "mode").getOrElse(JsNull).toString().replaceAll("\"", "")
+      val message = (json \ "postMessage").getOrElse(JsNull).toString()
+      // println(s"commmand from client: ${command}")
+      if (username.isEmpty){
+         username = (json \ "username").getOrElse(JsNull).toString().replaceAll("\"", "")
+      }
+
+      // println(s"username from client: ${username}")
       TCPsender = sender()
-      val responseDecoded = data.utf8String
+
+      if (mode == "get" ){
+      println("user in get mode")
+      val responseDecoded = command.replaceAll("\"", "")
       responseAsList= responseDecoded.split(" ").map(_.trim).toList
 
       firstWord = responseAsList.head
-        lastWord = responseAsList.last
+      lastWord = responseAsList.last
       if (firstWord == "subscribe") {
         for (i <- responseAsList){
           responseAsBuff.append(i)
@@ -87,19 +103,18 @@ case class ToClient(response: String)
         }
         for (id <- subscribedIDs) {
           val r = scala.util.Random
-          val keyGenerator = r.nextInt(100)
+          val keyGenerator = r.nextInt(1000)
           globalAckKey = keyGenerator
           val testString = s"{\n    \"key\": ${keyGenerator},\n    \"message\": \"${id}\"\n}"
           TCPsender ! Write(ByteString(testString))
           Thread.sleep(1000)
+          idList.append(keyGenerator)
         }
         context.actorSelection("akka://brokerSystem/user/queueManager").tell(getMessagesRequest(subscribedIDs, responseAsBuff, username), context.self)
       }
       else if (firstWord == "update") {
         for (a <- 1 to count){
           Message.get(a, s"toRecover/${username}").map(response => {
-//            println(s"sss  ${response.id}")
-
             if (!subscribedIDs.contains(response.id) && response.topic.nonEmpty){
               subscribedIDs.append(response.id)
             }
@@ -111,20 +126,20 @@ case class ToClient(response: String)
           })
         }
       }
-      else if (responseDecoded.forall(Character.isDigit)) {
-        if (responseDecoded.toInt == globalAckKey) {
-          println(s"message with key ${globalAckKey} successfully received by client")
+      else if (data.utf8String.forall(Character.isDigit)) {
+        if (idList.contains(data.utf8String.toInt)) {
+          println(s"client received message with id: ${data.utf8String.toInt}")
+          idList --= Seq(data.utf8String.toInt)
         }
       }
       else{
         if (username.nonEmpty){
           TCPsender ! Write(ByteString("unknown command"))
         } else {
-          username = data.utf8String
-          println(s"successfully set name to ${username}")
+          // username = data.utf8String
+         
           for (a <- 1 to count){
             Message.get(a, s"toRecover/${username}").map(response => {
-//              println(s"sss  ${response.id}")
               subscribedIDs.append(response.id)
               subscribedIDs.distinct
               println(s"updated list  ${subscribedIDs.toString}")
@@ -133,6 +148,15 @@ case class ToClient(response: String)
         }
 
       }
+    } else if (mode == "post" && message.nonEmpty){
+      println("user in post mode")
+      val json: JsValue = Json.parse(message)
+        def topic: String = {
+          (json \ "message" \ "tweet" \ "user" \ "time_zone").getOrElse(JsNull).toString()
+        }
+        context.actorSelection("akka://brokerSystem/user/queueManager").tell(sendTopic(topic), sender = context.self)
+        context.actorSelection("akka://brokerSystem/user/queueManager").tell(sendBody(message), sender = context.self)
+    }
       println(s"Data received - ${data.utf8String}")
     }
 
--- consumer/src/main/scala/cons/Main.scala
+++ consumer/src/main/scala/cons/Main.scala
@@ -10,18 +10,81 @@ object Main {
   def main(args: Array[String]): Unit = {
     val host = "localhost"
     val port = 5601
+    println("-----------------------------------------------")
     println(s"Started client! connecting to ${host}:${port}")
+    println("-----using mode(post/get). to change the mode use command \"mode <post>\" or \"mode <get>\".-----")
+    println("-----------------------------------------------")
 
+    var username = scala.io.StdIn.readLine(s"choose a username: ")
+    if (username.isEmpty){
+      username = scala.io.StdIn.readLine(s"Please try again: ")
+      // println(s"name successfully set to ${username}")
+    }
+    if (username.nonEmpty){
+      //  username = scala.io.StdIn.readLine(s"Please try again: ")
+      println(s"name successfully set to ${username}")
+    }
     val clientProps = TcpClient.props(new InetSocketAddress(host, port), null)
-
+    var mode = "get"
     val actorSystem: ActorSystem = ActorSystem.create("ConsumersActorSystem")
     val clientActor: ActorRef = actorSystem.actorOf(clientProps)
-
+    var postMessage = ""
+    var postCommand = ""
+    // println(s"SERVER_RES: Client connected to server \nUse commands bellow to make requests\n - subscribe <topicA> <topicB> ... -- to subscribe to specified topics, use it to add more topics to the subscribed list\n - unsubscribe <topic> \n - update -- to update \n - get topics -- to get existent topics \n - get messages -- to get messages for the subscribed topics")
+    
     Thread.sleep(2000)
     while (true){
-      val input = scala.io.StdIn.readLine("command> ")
+      
+      var responseAsList: List[String] = List[String]()
+      val r = scala.util.Random
+      // println(s"current mode is: ${mode}")
+      val input = scala.io.StdIn.readLine(s"mode ${mode}> ")
       if (input != null){
-        clientActor ! ByteString(input)
+        responseAsList= input.split(" ").map(_.trim).toList
+        // println(s"client input: ${responseAsList}")
+        if (responseAsList.head == "post" && mode == "post"){
+          val topic = scala.io.StdIn.readLine(s"enter the timezone")
+          val name = scala.io.StdIn.readLine(s"enter the screen name")
+          val keyGenerator = r.nextInt(1000)
+          val temp = s"{ \"message\": { \"tweet\": { \"user\": { \"time_zone\": \"${topic}\", \"screen_name\": \"${name}\" } } } }"
+          val testString = s"\n{\n    \"key\": ${keyGenerator},\n    \"postMessage\": ${temp},\n    \"command\": \"${input}\", \n    \"mode\": \"${mode}\",\n    \"username\": \"${username}\"\n}"
+          clientActor ! ByteString(testString)
+//          val temp = s"\n{ \"message\": { \"tweet\": { \"user\": { \"time_zone\": \"${topic}\", screen_name: \"${name}\" } } } }"
+//          clientActor ! ByteString(temp)
+        }
+
+
+        if (responseAsList.head == "mode"){
+          // println(s"currentm mode is: ${mode}")
+
+
+          if (responseAsList.last == "post"){
+            if(responseAsList.last == mode ){
+            println(s"mode is already: ${mode}")
+          }else{
+            mode = "post"
+            println(s"mode changed to: ${mode}")
+         }}
+         else if (responseAsList.last == "get" ){
+          if(responseAsList.last == mode ){
+            println(s"mode is already: ${mode}")
+          }else{
+            val keyGenerator = r.nextInt(1000)
+            mode = "get"
+            println(s"mode changed to: ${mode}")
+            // val testString = s"\n{\n    \"key\": ${keyGenerator},\n    \"postMessage\": ${postMessage},\n    \"command\": \"${input}\", \n    \"mode\": \"${mode}\",\n    \"username\": \"${username}\"\n}"
+            // clientActor ! ByteString(testString)
+          }
+          }
+          else {
+            println("no such mode! \neither use <post> to post, either <get> to get messages")
+          }
+        }
+        if(responseAsList.head == "get" || responseAsList.head == "subscribe" || responseAsList.head == "unsubscribe" || responseAsList.head == "update") {
+          val keyGenerator = r.nextInt(1000)
+          val testString = s"\n{\n    \"key\": ${keyGenerator},\n    \"postMessage\": \"${postMessage}\",\n    \"command\": \"${input}\", \n    \"mode\": \"${mode}\",\n    \"username\": \"${username}\"\n}"
+          clientActor ! ByteString(testString)
+        }
       }
     }
 
